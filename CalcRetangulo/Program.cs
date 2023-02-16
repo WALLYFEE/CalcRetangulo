d0e57d3afcf287d2fb0d662d501c87412ac40d4e
WALLYFEE <wallyfeealves@gmail.com>
2023-02-16 12:25:30 -0300

    alterações

---
+++ CalcRetangulo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalcRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+            Console.Write("Largura do retângulo: ");
+            retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Altura do retângulo: ");
+            retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(retangulo);
+        }
+    }
+}
---
+++ CalcRetangulo/Retangulo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalcRetangulo
+{
+    internal class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+
+        public double CalcArea()
+        {
+            return Largura * Altura;
+        }
+        public double CalcPerimetro()
+        {
+            return 2 * (Largura + Altura);
+          
+        }
+        public double CalcDiagonal()
+        {
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+            
+        }
+
+        public override string ToString()
+        {
+            return "Area: "
+                + CalcArea().ToString("F2", CultureInfo.InvariantCulture)
+                + "\nPerímetro: "
+                + CalcPerimetro().ToString("F2", CultureInfo.InvariantCulture)
+                + "\nDiagonal: "
+                + CalcDiagonal().ToString("F2", CultureInfo.InvariantCulture);
+
+
+        }
+    }
+}
